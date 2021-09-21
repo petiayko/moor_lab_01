d7aa188e8bac1e49c2c26f9010866a1eddcacfc8
Peter <p.makretskii@gmail.com>
2021-09-21 23:38:33 +0300

    v1.0

---
+++ main.py
@@ -0,0 +1,20 @@
+def read(path):
+    data = open(path, 'r')
+    table = []
+    for line in data:
+        if '|' in line:
+            line_list = line.split(' ')
+            table.append((
+                [int(val) for val in line_list[:line_list.index('|')]],
+                int(line_list[len(line_list) - 1].replace('\n', ''))
+            ))
+    objective = table[len(table) - 1]
+    del table[len(table) - 1]
+
+    return table, objective
+
+
+if __name__ == '__main__':
+    coef, obj = read('input.txt')
+    print(coef)
+    print(obj)
